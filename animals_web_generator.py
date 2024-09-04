723d05308dd908cff24e796beb739074ea754b37
hsfpu5h <helge.friedel@gmail.com>
2024-09-04 20:08:47 +0200

    bring animal data into html

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,30 +1,52 @@
 import json
 
 
-def load_data(file_path):
+def load_html_data(file_path_html):
   """ Loads a JSON file """
-  with open(file_path, "r") as handle:
-    return json.load(handle)
+  with open(file_path_html, "r") as handle:
+    html_data = handle.read()
+    return html_data
 
 
-def parse_data(animals_data):
-  for animal in animals_data:
-    print(f"Name: {animal["name"]}")
-    print(f"Diet: {animal["characteristics"]["diet"]}")
-    print(f"animal["locations"][0])
+def load_json_data(file_path_json):
+  """ Loads a JSON file """
+  with open(file_path_json, "r") as handle:
+    json_data = json.load(handle)
+    return json_data
+
+
+def parse_json_data(json_data):
+  output = ""
+  for animal in json_data:
+    output += f'\nName: {animal["name"]}\n'
+    output += f'Diet: {animal["characteristics"]["diet"]}\n'
+
+    if animal["locations"]:
+      first_location = animal["locations"][0]
+      output += f'Location: {first_location}\n'
 
     if "type" in animal["characteristics"]:
-      print(animal["characteristics"]["type"])
+      output += f'{animal["characteristics"]["type"]}\n'
     else:
-      print("Type not available")
+      output += f'Type not available.\n\n'
+  return output
 
-    print()
+
+def replace_animals_info(html_template, animals_info):
+    return html_template.replace('__REPLACE_ANIMALS_INFO__', animals_info)
+
+
+def write_to_file(file_path, content):
+  with open(file_path, "w") as handle:
+    handle.write(content)
 
 
 def main():
-  animals_data = load_data('animals_data.json')
-  print(animals_data)
-  parse_data(animals_data)
+  json_data = load_json_data("animals_data.json")
+  html_template = load_html_data("animals_template.html")
+  animals_info = parse_json_data(json_data)
+  new_html_content = replace_animals_info(html_template, animals_info)
+  write_to_file("animals_template.html", new_html_content)
 
 
 if __name__ == "__main__":
